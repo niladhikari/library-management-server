4f1ad788881b4bd2361c3394739e2997851cdb73
niladhikari <niladhikari445@gmail.com>
2023-11-05 11:38:49 +0600

    add the server side set up,,install the expres cors for set the enviroments..than require the express and cors then add the app and port and use the middlewair. Then respond send the server

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express");
+const cors = require("cors");
+
+require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+ 
+
+app.get('/', (req, res) => {
+    res.send('Library Management Server')
+  })
+  
+  app.listen(port, () => {
+    console.log(`Library Management Server listening on port ${port}`)
+  })
